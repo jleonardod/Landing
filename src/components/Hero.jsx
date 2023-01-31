693998695ee06d3cfe09cf53815f80744ae41594
Leonardo Diaz <jldia9623@gmail.com>
2023-01-30 22:17:11 -0500

    Hero sin responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,88 @@
 import React from "react"
+import { RiCheckboxBlankCircleFill, RiCheckboxBlankFill, RiPlayFill, RiStarFill } from "react-icons/ri"
 
 const Hero = () => {
   return(
-    <section className="bg-red-300 h-[90vh]">Hero</section>
+    <section className="h-[90vh] grid grid-cols-1 md:grid-cols-8">
+      {/* Information */}
+      <div className="md:col-span-5 flex items-center justify-center p-16">
+        <div className="flex flex-col gap-8">
+          <h1 className="text-6xl font-bold leading-[6.5rem]">
+            Web Design Impactful Digital {" "}
+            <span className="text-primary py-0 px-6 border-8 border-primary relative">
+              Products
+              <RiCheckboxBlankCircleFill className="text-white text-base absolute -left-5 -top-5 p-2 bg-primary rounded-full box-content" />
+              <RiCheckboxBlankCircleFill className="text-white text-base absolute -right-5 -top-5 p-2 bg-primary rounded-full box-content" />
+              <RiCheckboxBlankCircleFill className="text-white text-base absolute -right-5 -bottom-5 p-2 bg-primary rounded-full box-content" />
+              <RiCheckboxBlankCircleFill className="text-white text-base absolute -left-5 -bottom-5 p-2 bg-primary rounded-full box-content" />
+            </span>
+          </h1>
+          <p className="text-gray-500 text-2xl leading-[2.5rem]">
+            Help find solutions with institutive and in accordance with client business goals. We provide a high-quality services.
+          </p>
+          <div className="flex items-center gap-4">
+            <button className="bg-primary text-white py-2 px-8 rounded-xl text-xl font-bold">
+              Contact Us
+            </button>
+            <button className="flex items-center justify-start text-left gap-4 py-2 px-8 rounded-xl text-xl text-gray-500">
+              <RiPlayFill className="bg-secondary text-primary p-4 rounded-full box-content" /> Watch our <br /> introduction video
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* Image */}
+      <div className="md:col-span-3 flex items-center justify-center relative">
+        {/* Content image */}
+        <div>
+          <img src="hero.png" className="w-[450px] h-[450px] object-cover -mt-28" />
+          {/*<RiCheckboxBlankCircleFill className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 text-[380px] text-secondary bg-primary rounded-full -z-10" />*/}
+          <div className="relative bg-white shadow-xl p-2 flex flex-col justify-center max-w-[250px] mx-auto gap-2 -mt-12">
+            <div className="flex items-center">
+              <img 
+                src="https://img.freepik.com/foto-gratis/empresaria-confiada-sonriente-que-presenta-brazos-cruzados_1262-20950.jpg" 
+                className="w-10 h-10 object-cover rounded-full ring-2 ring-gray-300"/>
+              <img 
+                src="https://img.freepik.com/foto-gratis/prospero-hombre-negocios-mantiene-manos-cruzadas-tiene-expresion-satisfecha_273609-16711.jpg" 
+                className="w-10 h-10 object-cover rounded-full ring-2 ring-gray-300 -ml-4"/>
+              <img 
+                src="https://img.freepik.com/foto-gratis/retrato-hermoso-mujer-joven-posicion-pared-gris_231208-10760.jpg" 
+                className="w-10 h-10 object-cover rounded-full ring-2 ring-gray-300 -ml-4"/>
+              <img 
+                src="https://img.freepik.com/foto-gratis/retrato-joven-sonriente-gafas_171337-4842.jpg" 
+                className="w-10 h-10 object-cover rounded-full ring-2 ring-gray-300 -ml-4"/>
+              <img 
+                src="https://img.freepik.com/foto-gratis/vista-lateral-hermosa-mujer-joven-piel-limpia-saludable-aspecto-natural-maquillaje-mirando-izquierda-sonriendo-pie-sobre-pared-blanca_176420-34093.jpg" 
+                className="w-10 h-10 object-cover rounded-full ring-2 ring-gray-300 -ml-4"/>
+              <img 
+                src="https://img.freepik.com/fotos-premium/representacion-3d-personaje-masculino-barbudo-medio-cuerpo-anteojos-franela-roja_477250-77.jpg" 
+                className="w-10 h-10 object-cover rounded-full ring-2 ring-gray-300 -ml-4"/>
+            </div>
+            <h2 className="font-bold text-xl tracking-[-1px] text-gray-800">120 + Employees</h2>
+            <div className="flex items-center gap-2 text-lg text-gray-500">
+              <RiStarFill className="text-primary" /> 5.0 (3.1k Reviews)
+            </div>
+            <div className="absolute -right-12 -bottom-12 -z-10">
+              <div className="relative">
+                <RiCheckboxBlankCircleFill className="text-primary text-8xl" />
+                <div className="absolute left-0 top-0 bg-white w-14 h-14"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Circle */}
+        <div className="rounded-full p-44 bg-secondary absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 border-primary border-8 -z-10"></div>
+
+        {/* Logos */}
+        <img
+          src="figma.png" 
+          className="w-20 h-20 object-cover rounded-full border-l-8 border-gray-600 absolute top-[8%] right-[10%] -rotate-6"
+        />
+        <img
+          src="adobe.png" 
+          className="w-14 h-14 object-cover rounded-full border-l-8 border-gray-600 absolute top-[7.5%] left-[19%] -rotate-12"
+        />
+      </div>
+    </section>
   )
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,7 @@ module.exports = {
     extend: {
       colors: {
         primary: "#706FE5",
+        secondary: "#EAEAFC",
       }
     },
   },
