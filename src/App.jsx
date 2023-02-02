1837181aa7a29dd522c268a6d4536fea024e29b6
Leonardo Diaz <jldiaz9623@gmail.com>
2023-02-02 11:25:41 -0500

    Section Works finalizada responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Clients from "./components/Clients"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import Works from "./components/Works"
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Hero />
       <Clients />
+      <Works />
     </div>
   )
 }
