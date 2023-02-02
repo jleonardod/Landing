d94b7e8accba9d75c791f4b2c9059a0a0459d843
Leonardo Diaz <jldiaz9623@gmail.com>
2023-02-02 10:22:53 -0500

    Section clients terminada responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Clients from "./components/Clients"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Header />
       <Hero />
+      <Clients />
     </div>
   )
 }
